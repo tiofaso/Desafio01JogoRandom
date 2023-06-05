a9b0694cb2a719f290563954d0df0dd6c5d63ea9
tiofaso <fabio.vaz@zup.com.br>
2023-06-05 13:58:28 -0300

    Primeiro desafio do catalisa

---
+++ JogoAleatorio.java
@@ -0,0 +1,141 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class JogoAleatorio {
+    public static void main(String[] args){
+        System.out.println("---------------------------------------");
+        System.out.println("| Vamos brincar de adivinhar números? |");
+        System.out.println("---------------------------------------");
+
+        //Pegando número digitado pelo usuário
+        System.out.println("Escolha um dos números para selecionar a dificuldade:");
+        System.out.println("1 Easy  |  2 Medium  | 3 Hard  | 4 Joy has no mercy");
+        Scanner entrada = new Scanner(System.in);
+        int numeroUsuario = entrada.nextInt();
+
+        //verificando se o usuário fez uma opção válida
+        if(numeroUsuario < 1 || numeroUsuario > 4 ) {
+            int numErrado = 1;
+            while(numErrado == 1) {
+
+                System.out.println("---------------------------------------");
+                System.out.println("AVISO! É preciso escolher um grau de dificuldade válido.");
+                System.out.println("---------------------------------------\n");
+
+                System.out.println("Escolha um dos números para selecionar a dificuldade:");
+                System.out.println("1 Easy  |  2 Medium  | 3 Hard  | 4 Joy has no mercy");
+
+                numeroUsuario = entrada.nextInt();
+
+                if(numeroUsuario >= 1 && numeroUsuario <= 4){numErrado = 0;}
+
+            }
+        }
+
+        System.out.println("\n---------------------------------------");
+
+        //Definindo o grau de dificuldade da partida
+        int numDificuldade = numeroUsuario;
+        String msgNivel = "";
+
+        switch (numDificuldade){
+            case 1:
+                System.out.println("Você escolheu o nível EASY. Escolha entre 0 - 10 números");
+                msgNivel = "EASY";
+                break;
+            case 2:
+                System.out.println("Você escolheu o nível MEDIUM. Escolha entre 0 - 100 números");
+                msgNivel = "MEDIUM";
+                break;
+            case 3:
+                System.out.println("Você escolheu o nível HARD. Escolha entre 0 - 1000 números");
+                msgNivel = "HARD";
+                break;
+            case 4:
+                System.out.println("Você escolheu o nível JOY HAS NO MERCY. Escolha entre 0 - 10000 números");
+                msgNivel = "JOY HAS NO MERCY";
+                break;
+        }
+
+        int msgExit = 0; //flag para interromper o jogo
+        int numScore = 0; //score do jogo
+        int numJogadas = 0; //numero de vezes que o usuário jogou
+
+        //Histórico de jogadas do usuário e do computador
+        ArrayList<Integer> jogadaUsr = new ArrayList<>();
+        ArrayList<Integer> jogadaComp = new ArrayList<>();
+        ArrayList<Integer> jogadaPonto = new ArrayList<>();
+
+
+        while (msgExit == 0){
+
+            System.out.println("Digite um número [" + msgNivel + "]:");
+            numeroUsuario = entrada.nextInt();
+
+            //Separando o número digitado pelo usuário para comparação
+            int numeroUsuarioAntes = numeroUsuario - 1;
+            int numeroUsuarioDepois = numeroUsuario + 1;
+            int numPontosInstantaneo = 0; //flag para informar quanto o jogador ganhou naquele instante
+
+            //Gerando números aleatórios
+            int numRandom = (int) (Math.random() * (numeroUsuario+1));
+
+            //Adicionando dados ao histórico
+            jogadaUsr.add(numeroUsuario);
+            jogadaComp.add(numRandom);
+
+            //Comparando o que o usuário digitou com o que o computador gerou
+            if(numeroUsuario == numRandom){
+                numScore = numScore + 10;
+                numJogadas = numJogadas + 1;
+                numPontosInstantaneo = 10;
+                jogadaPonto.add(numPontosInstantaneo);
+            }else if(numeroUsuarioAntes == numRandom || numeroUsuarioDepois == numRandom){
+                numScore = numScore + 5;
+                numJogadas = numJogadas + 1;
+                numPontosInstantaneo = 5;
+                jogadaPonto.add(numPontosInstantaneo);
+            }else{
+                numJogadas = numJogadas + 1;
+                numPontosInstantaneo = 0;
+                jogadaPonto.add(numPontosInstantaneo);
+            }
+
+            System.out.println("Você digitou " + numeroUsuario);
+            System.out.println("O computador selecionou o número " + numRandom);
+            System.out.println("Você ganhou " + numPontosInstantaneo + " pontos");
+
+            System.out.println("\n\n> Deseja sair? [1 para sair / 0 para continuar]");
+            msgExit = entrada.nextInt();
+
+            if(msgExit > 1 ){msgExit = 0;}
+            else if(msgExit < 1 ){msgExit = 0;}
+
+
+
+        }
+
+        System.out.println("\n----------------------");
+        System.out.println("Seu score final é de " + numScore + " pontos");
+        System.out.println("As suas jogadas foram: " + jogadaUsr);
+        System.out.println("As jogadas do computador: " + jogadaComp);
+
+        //exibindo o histórico de jogadas e sua pontuação
+        int contHistoricoMax = jogadaUsr.size();
+        int contHistorico = 0;
+
+        System.out.println("\nHistórico de jogadas");
+        while(contHistorico < contHistoricoMax){
+            System.out.println("Jogada n°" + contHistorico);
+            System.out.println("Computador jogou: " + jogadaComp.get(contHistorico));
+            System.out.println("Você jogou: " + jogadaUsr.get(contHistorico));
+            System.out.println("Nessa jogada você recebeu "+ jogadaPonto.get(contHistorico)  + " pontos\n");
+            contHistorico++;
+        }
+
+
+
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+
+# Desafio 01 de Java
+
+Nesse desafio o usuário digita um número qualquer tentando advinhar qual o computador irá escolher aleatoriamente.
+
+Ele possui 4 níveis:
+
+- **Easy** O mais fácil dos modos. Só dez (10) opções para descobrir o que o computador pode escolher.
+- **Medium** O padrão dos modos. Apenas 100 (100) opções para descobrir o que o computador pode escolher.
+- **Hard**  O mais difícil sem muitos esxageros. São mil (1.000) opções para descobrir o que o computador pode escolher.
+- **Joy has no mercy** O mais difícil de todos os modos, feito para fazer até a sua mãe chorar. São dez mil (10.000) opções para descobrir o que o computador pode escolher.
+
+A cada jogada o usuário pode ganhar:
+- **10 (dez)** pontos se acertar em cheio;
+- **5 (cinco)** pontos se chegar pertinho; e
+- **0 (zero)** pontos se você for a *vergonha da profisson!*
+
+O sistema também guarda o seu histórico de jogadas, pontuação e o que o computador respondeu.
